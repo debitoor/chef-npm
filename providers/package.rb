4c146cd2eec6da15bf8cb224849e299f21cd1321
Jeppe Nejsum Madsen <jeppe@ingolfs.dk>
2014-03-21 16:55:52 +0100

    Added support for Node on Windows

--- providers/package.rb
+++ providers/package.rb
@@ -1,13 +1,37 @@
 # encoding: utf-8
 
+require 'chef/mixin/shell_out'
+include Chef::Mixin::ShellOut
+
+def get_npmcmd(args)
+  case node[:platform_family]
+  when 'windows'
+    npmcmd = '"C:/Program files/nodejs/npm"'
+  else
+    npmcmd = 'npm'
+  end
+  "#{npmcmd} #{args}"
+end
+
+
+def package_installed(pkg_id, cwd="")
+  cdcmd = cwd == "" ?  "" : "cd #{cwd} && "
+  npmcmd = get_npmcmd("--no-color -g ls #{pkg_id}")
+  cmd = "#{cdcmd}#{npmcmd}"
+  p = shell_out(cmd)
+  res = /#{pkg_id}/ =~ p.stdout 
+  Chef::Log.debug("#{cmd} exitstatus #{p.exitstatus}, output: #{p.stdout}, res: #{res}")
+  res
+end
+
 use_inline_resources if defined?(use_inline_resources)
 
 action :install do
   pkg_id = new_resource.name
   pkg_id += "@#{new_resource.version}" if new_resource.version
   execute "install NPM package #{new_resource.name}" do
-    command "npm -g install #{pkg_id}"
-    not_if "npm --no-color -g ls '#{pkg_id}' 2> /dev/null | grep ' #{pkg_id}'"
+    command get_npmcmd("-g install #{pkg_id}")
+    not_if {package_installed(pkg_id)}
   end
 end
 
@@ -17,17 +41,16 @@ action :install_local do
   pkg_id += "@#{new_resource.version}" if new_resource.version
   execute "install NPM package #{new_resource.name} into #{path}" do
     cwd path
-    command "npm install #{pkg_id}"
-    not_if "cd #{path} && npm --no-color ls '#{pkg_id}' 2> /dev/null | grep ' #{pkg_id}'"
+    command get_npmcmd("install #{pkg_id}")
+    not_if {package_installed(pkg_id, path)}
   end
 end
 
 action :install_from_json do
   path = new_resource.path
-  cmd  = 'npm install'
   execute "install NPM packages from package.json at #{path}" do
     cwd path
-    command cmd
+    command get_npmcmd("install")
   end
 end
 
@@ -35,8 +58,8 @@ action :uninstall do
   pkg_id = new_resource.name
   pkg_id += "@#{new_resource.version}" if new_resource.version
   execute "uninstall NPM package #{new_resource.name}" do
-    command "npm -g uninstall #{pkg_id}"
-    only_if "npm --no-color -g ls '#{pkg_id}' 2> /dev/null | grep ' #{pkg_id}'"
+    command get_npmcmd("-g uninstall #{pkg_id}")
+    only_if {package_installed(pkg_id)}
   end
 end
 
@@ -46,7 +69,7 @@ action :uninstall_local do
   pkg_id += "@#{new_resource.version}" if new_resource.version
   execute "uninstall NPM package #{new_resource.name} from #{path}" do
     cwd path
-    command "npm uninstall #{pkg_id}"
-    only_if "cd #{path} && npm --no-color ls '#{pkg_id}' 2> /dev/null | grep ' #{pkg_id}'"
+    command get_npmcmd("uninstall #{pkg_id}")
+    only_if {package_installed(pkg_id, path)}
   end
 end
